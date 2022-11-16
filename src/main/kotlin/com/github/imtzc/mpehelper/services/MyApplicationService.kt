2798c1d230b6574fcf3f4e7c0ec2c66a672cc780
GitHub Action <action@github.com>
2022-11-16 05:51:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/imtzc/mpehelper/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/imtzc/mpehelper/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.imtzc.mpehelper.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.imtzc.mpehelper.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/imtzc/mpehelper/services/MyProjectService.kt
+++ src/main/kotlin/com/github/imtzc/mpehelper/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.imtzc.mpehelper.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.imtzc.mpehelper.MyBundle
 
 class MyProjectService(project: Project) {
 
